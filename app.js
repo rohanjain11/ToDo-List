7961485fc31c70b87b1783df288a45911d299a47
Rohan Jain <108605232+rohanjain11@users.noreply.github.com>
2023-03-21 20:10:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -134,6 +134,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(3000 || process.env.PORT, ()=>{
+    console.log(`The application started successfully on port ${port}`);
 });
